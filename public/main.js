90e02ac55ae70fe5d627cc4521488b1898166b4f
Alexander Vladimirov <alexander@vladimirov.msk.ru>
2025-02-03 16:37:17 +0500

    feat(Popup, bootstrap, clipboard): Add bootstrap styles to submit form, copy shortLink to clipboard and show popup message

--- public/main.js
+++ public/main.js
@@ -1,9 +1,29 @@
+const URL_PATTERN = `^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$`;
+
 function debounce(fn, t = 500) {
   let timeout;
   return function (...args) {
     clearTimeout(timeout);
     timeout = setTimeout(fn, t, ...args);
   };
+}
+
+function showPopup() {
+  const popupEl = document.getElementById('popupText');
+  popupEl.classList.add('show');
+}
+
+function hidePopup() {
+  const popupEl = document.getElementById('popupText');
+  popupEl.classList.remove('show');
+}
+
+const debouncedHidePopup = debounce(hidePopup, 5000);
+
+async function copyToClipboard(shortLink) {
+  if (!shortLink) return Promise.reject();
+
+  return await navigator.clipboard.writeText(shortLink);
 }
 
 let inputText = null;
@@ -19,30 +39,26 @@ function clickHandler(url) {
     body: JSON.stringify({ origUrl: inputText }),
   })
     .then((response) => response.json())
-    .then((result) => {
-      linkEl = document.createElement('a');
-      linkEl.innerText = result?.shortUrl || null;
-      linkEl.href = result?.shortUrl || null;
-      linkEl.id = 'shortUrl';
+    .then(async (result) => {
+      const { shortUrl } = result;
+      shortLinkEl.value = shortUrl || null;
 
-      shortLinkEl.replaceChildren(linkEl);
+      await copyToClipboard(shortUrl).then(() => {
+        showPopup();
+        debouncedHidePopup();
+      });
     });
 
-  window.addEventListener('unload', () => {
-    linkEl?.remove();
-  });
-
   buttonEl.disabled = true;
-  linkInputEl.classList.remove('valid-input');
+  linkInputEl.classList.remove('is-valid');
   linkInputEl.value = null;
   inputText = null;
 }
 
-const URL_PATTERN = `^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$`;
-
 document.addEventListener('DOMContentLoaded', () => {
   const linkInputEl = document.getElementById('inputLink');
   const buttonEl = document.getElementById('submitButton');
+  const shortLinkEl = document.getElementById('shortLink');
 
   linkInputEl.addEventListener(
     'input',
@@ -51,16 +67,22 @@ document.addEventListener('DOMContentLoaded', () => {
       const isValid = regex.test(e.target.value);
 
       if (isValid) {
-        linkInputEl.classList.add('valid-input');
-        linkInputEl.classList.remove('invalid-input');
+        linkInputEl.classList.add('is-valid');
+        linkInputEl.classList.remove('is-invalid');
       } else {
-        linkInputEl.classList.add('invalid-input');
-        linkInputEl.classList.remove('valid-input');
+        linkInputEl.classList.add('is-invalid');
+        linkInputEl.classList.remove('is-valid');
       }
 
       buttonEl.disabled = !isValid;
 
       inputText = e.target.value;
+      shortLinkEl.value = null;
+      hidePopup();
     })
   );
+
+  window.addEventListener('unload', () => {
+    if (shortLinkEl && shortLinkEl?.value) shortLinkEl.value = null;
+  });
 });
